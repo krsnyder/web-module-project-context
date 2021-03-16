2d8928bf077af006ecf3176d8cd914b49e73e521
Kirk <kirkrobertsnyder@gmail.com>
2021-03-16 15:36:13 -0400

    Consumed data with ProductContext

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function App() {
 
 	return (
 		<div className="App">
-			<ProductContext.Provider >
+			<ProductContext.Provider value={{products, addItem}}>
 				<Navigation cart={cart} />
 
 				{/* Routes */}
 				<Route exact path="/">
-					<Products products={products} addItem={addItem} />
+					<Products/>
 				</Route>
 
 				<Route path="/cart">
